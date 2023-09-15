70f926625a6bed4de28016c4be9c74ddbf1b5d2d
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-15 03:40:53 -1200

    added service and products page

--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-const products = () => {
-  return (
-    <h1 className="text-9xl font-xlxlbold underline ">
-      products
-    </h1>
-  )
-}
+const products = [
+  {
+    title: 'Product 1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/images/bg-1.png',
+  },
+  {
+    title: 'Product 2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/images/bg-1.png',
+  },
+  {
+    title: 'Product 3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/images/bg-1.png',
+  },
+  {
+    title: 'Product 4',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/images/bg-1.png',
+  },
+  // Add more products...
+];
 
-export default products
+const ProductCard = ({ title, description, image }) => (
+  <div className="bg-white shadow-lg rounded-lg overflow-hidden transform hover:-translate-y-1 hover:shadow-2xl transition-transform duration-300">
+    <img className="w-full h-48 object-cover" src={image} alt={title} />
+    <div className="p-6">
+      <h3 className="text-xl font-bold mb-2">{title}</h3>
+      <p className="text-gray-700">{description}</p>
+    </div>
+  </div>
+);
+
+const Products = () => (
+  <div className="container mx-auto py-10">
+    <h1 className="text-4xl font-bold text-center mb-10">Our Products</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      {products.map((product, index) => (
+        <ProductCard key={index} {...product} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Products;
--- pages/services.js
+++ pages/services.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from 'react';
 
-const services = () => {
-  return (
-    <h1 className="text-9xl font-xlxlbold underline ">
-    services
-  </h1>
-  )
-}
+const services = [
+  {
+    title: 'Web Development',
+    description: 'We build modern and responsive websites tailored to your needs.',
+    icon: 'fas fa-laptop-code',
+  },
+  {
+    title: 'Mobile App Development',
+    description: 'We create native and cross-platform mobile applications for iOS and Android.',
+    icon: 'fas fa-mobile-alt',
+  },
+  {
+    title: 'UI/UX Design',
+    description: 'We design intuitive and visually appealing user interfaces and experiences.',
+    icon: 'fas fa-paint-brush',
+  },
+  {
+    title: 'Cloud Consulting',
+    description: 'We provide expert guidance and support for leveraging cloud technologies.',
+    icon: 'fas fa-cloud',
+  },
+  {
+    title: 'Data Analytics',
+    description: 'We analyze and interpret data to help you make informed business decisions.',
+    icon: 'fas fa-chart-line',
+  },
+  {
+    title: 'IT Security',
+    description: 'We offer comprehensive solutions to protect your digital assets and data.',
+    icon: 'fas fa-shield-alt',
+  },
+  {
+    title: 'Blockchain Development',
+    description: 'We develop secure and decentralized blockchain applications.',
+    icon: 'fas fa-cubes',
+  },
+];
 
-export default services
+const ServiceCard = ({ title, description, icon }) => (
+  <div className="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300 flex flex-col items-center">
+    <i className={`${icon} text-4xl text-blue-500 mb-4`} />
+    <h3 className="text-xl font-bold mb-2">{title}</h3>
+    <p className="text-gray-700">{description}</p>
+  </div>
+);
+
+const Services = () => (
+  <div className="container mx-auto py-10">
+    <h1 className="text-4xl font-bold text-center mb-10">Our Services</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {services.map((service, index) => (
+        <ServiceCard key={index} {...service} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Services;
