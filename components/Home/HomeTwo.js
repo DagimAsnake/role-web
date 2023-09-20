2af8b765af62a6ed43e4b02e6344a151be946658
Dagim Asnake <dagimasnake1@gmail.com>
2023-09-19 23:21:49 -1200

    added cloud on hometwo

--- components/Home/HomeTwo.js
+++ components/Home/HomeTwo.js
@@ -25,26 +25,29 @@ const HomeTwo = () => {
 
                 <div className="flex flex-col items-center bg-gray-200 rounded-lg p-8 py-24 mt-8 hover:bg-gray-300 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" />
                     </svg>
-                    <h3 className="text-2xl font-bold mt-4">Event Support</h3>
-                    <p className="text-lg mt-2">We provide Event Registration Builds, Cvent support, and Live Helpdesk Support for virtual events.</p>
+
+                    <h3 className="text-2xl font-bold mt-4">Cloud Migration and Consulting</h3>
+                    <p className="text-lg mt-2">Offer expertise in migrating existing systems and applications to the cloud, as well as providing guidance on cloud adoption strategies and best practices.</p>
                 </div>
 
                 <div className="flex flex-col items-center bg-gray-200 rounded-lg p-8 py-24 mt-8 hover:bg-gray-300 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                     </svg>
-                    <h3 className="text-2xl font-bold mt-4">IT Helpdesk</h3>
-                    <p className="text-lg mt-2">We provide 24/7 Tier 1IT support for your employees through phone, email, and live chat.</p>
+
+                    <h3 className="text-2xl font-bold mt-4">Cloud Backup and Disaster Recovery</h3>
+                    <p className="text-lg mt-2"> Provide cloud-based backup and disaster recovery solutions, ensuring business continuity and data protection in the event of a system failure or natural disaster.</p>
                 </div>
 
                 <div className="flex flex-col items-center bg-gray-200 rounded-lg p-8 py-24 mt-8 hover:bg-gray-300 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z" />
                     </svg>
-                    <h3 className="text-2xl font-bold mt-4">Representatives</h3>
-                    <p className="text-lg mt-2">Support your business with friendly and professional sales, call center, & bespoke resources.</p>
+
+                    <h3 className="text-2xl font-bold mt-4">Serverless Computing</h3>
+                    <p className="text-lg mt-2">Develop and deploy applications using serverless architectures, which allow businesses to focus on their core functions without worrying about infrastructure management.</p>
                 </div>
 
             </div>
